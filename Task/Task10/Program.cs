07f20a1ddd6a1520fdb0a3ea48b1982cbd67319b
KIM <kim.02@mail.ru>
2022-09-21 18:45:39 +0300

    З 10,13,15

--- Task/Task10/Program.cs
+++ Task/Task10/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 Console.WriteLine("Hello, World!");
--- Task/Task13/Program.cs
+++ Task/Task13/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 Console.WriteLine("Hello, World!");

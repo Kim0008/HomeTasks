b8b1b8a41860e54270330ac8441488d74fde2d33
KIM <kim.02@mail.ru>
2022-09-22 21:16:52 +0300

    задача 10

--- Task/Task10/Program.cs
+++ Task/Task10/Program.cs
@@ -1,2 +1,12 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-Console.WriteLine("Hello, World!");
+
+int Dozens;
+Console.WriteLine("Введите число:");
+Dozens = Convert.ToInt32(Console.ReadLine());
+int DelSecondDigit(int Dozens)
+{
+    int nDozens = (Dozens / 10) % 10;
+        return nDozens;
+}
+int N = DelSecondDigit(Dozens);
+Console.WriteLine("Вторая цыфра: " + N);

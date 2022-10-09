d84153d257f216951880904ddcaf7d8a9dd364a6
KIM <kim.02@mail.ru>
2022-10-09 21:31:44 +0300

    задача 36

--- Task/Task36/Program.cs
+++ Task/Task36/Program.cs
@@ -2,7 +2,6 @@
 
 Console.WriteLine("Введите размер массива: ");
 int numbers = Convert.ToInt32(Console.ReadLine());
-
 void FillArray(int[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
@@ -10,7 +9,6 @@ void FillArray(int[] arr)
         arr[i] = new Random().Next(1, 100);
     }
 }
-
 void PrintArray(int[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
@@ -18,18 +16,14 @@ void PrintArray(int[] arr)
         Console.Write(arr[i] + " ");
     }
     Console.WriteLine();
-
     int sum = 0;
-
     for (int j = 1; j < arr.Length; j += 2)
     {
         sum = sum + arr[j];
 
     }
-
     Console.WriteLine($"всего {arr.Length} чисел, сумма на нечетных позициях = {sum}");
 }
-
 int[] Yourmassiv = new int[numbers];
 FillArray(Yourmassiv);
 PrintArray(Yourmassiv);
